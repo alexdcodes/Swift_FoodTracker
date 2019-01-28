a1307d84956ee0ddc50f7a130b1a469d8b74fcad
Alex Diker <alex.diker@live.ca>
2019-01-27 22:42:57 -0500

    First upload,
    Will add more obviously, just starting to follow the tutorial.

---
+++ Food_Tracker/Food_Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food_Tracker
+//
+//  Created by Admin on 2019-01-27.
+//  Copyright © 2019 AlexD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
