6c14948dbd837c634d9b4c15be898fa7e0a9d8cf
Alex Diker <alex.diker@live.ca>
2019-01-27 23:19:07 -0500

    Add files via upload

--- Food_Tracker/Food_Tracker/ViewController.swift
+++ Food_Tracker/Food_Tracker/ViewController.swift
@@ -11,13 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     //MARK: Properties
 
+    @IBOutlet weak var mealNameLabel: UITextField!
+    
     @IBOutlet weak var nameTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    // MARK: ACTION
 
-
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    }
 }
 
